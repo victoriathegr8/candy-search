b29a8365842733fd9d54f2fad3503f26cc9620fc
Roshni Srikanth <rsrika@uw.edu>
2021-06-09 17:56:16 -0700

    added pseudocode

--- src/components.js
+++ src/components.js
@@ -35,12 +35,17 @@ export function Header() {
   }
  
 
-  
+
 // creates the signin page
 export function SignIn(props) {
+  // make sure to make the addedCandies State, and bring the value into this method
+  function handleSignIn() {
+    props.setSignedIn(true)
+    props.setAddedCandies()//added candies from firebase);
+  }
   return (
     <div className="container">
-      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} onClick={props.setSignedIn(true)} />
+      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} onClick={() => {handleSignIn()}}/>
     </div>
   );
 }
