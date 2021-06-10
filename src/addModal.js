caed9f8a354b1a3f7f62074deff8fe7f37a828ad
Roshni Srikanth <rsrika@uw.edu>
2021-06-09 20:51:37 -0700

    fixed the nav bar problem

--- src/addModal.js
+++ src/addModal.js
@@ -49,51 +49,51 @@ export function AddModal(props) {
             <div className="modal-content card-body">
                 <span className="add-modal-close close" onClick={() => props.handleModalClose()}>×</span>
                 <span className="add-modal-txt">Add Your New Candy Information</span>
-                <label for="name">Name of your Candy: </label>
+                <label>Name of your Candy: </label>
                 <input type="text" name="name" onChange={(event) => {candy.competitorname = event.target.value}}></input>
                 <br/>
-                <label for="image">Image Url of a Picture of your Candy: </label>
+                <label >Image Url of a Picture of your Candy: </label>
                 <input type="text" name="image" onChange={(event) => {candy.imglink = event.target.value}}></input>
                 <br/>
                 <input type="checkbox" name="containsEgg" onClick={() => {candy.hasegg = 1}}></input>
-                <label for="containsEgg">Your Candy contains Egg</label>
+                <label >Your Candy contains Egg</label>
                 <br/>
                 <input type="checkbox" name="containsMilk" onClick={() => {candy.hasmilk = 1}}></input>
-                <label for="containsMilk">Your Candy contains Milk</label>
+                <label >Your Candy contains Milk</label>
                 <br/>
                 <input type="checkbox" name="containsSoy" onClick={() => {candy.hassoy = 1}}></input>
-                <label for="containsSoy">Your Candy contains Soy</label>
+                <label >Your Candy contains Soy</label>
                 <br/>
                 
                 <input type="checkbox" name="chocolate" onClick={() => {candy.chocolate = 1}}></input>
-                <label for="containsChocolate">Your Candy contains Chocolate</label>
+                <label >Your Candy contains Chocolate</label>
                 <br/>
                 <input type="checkbox" name="fruity" onClick={() => {candy.fruity = 1}}></input>
-                <label for="fruity">Your Candy is Fruity</label>
+                <label >Your Candy is Fruity</label>
                 <br/>
                 <input type="checkbox" name="caramel" onClick={() => {candy.caramel = 1}}></input>
-                <label for="caramel">Your Candy contains caramel</label>
+                <label >Your Candy contains caramel</label>
                 <br/>
                 <input type="checkbox" name="peanutyalmondy" onClick={() => {candy.peanutyalmondy = 1}}></input>
-                <label for="peanutyalmondy">Your Candy contains peanuts, almonds, or peanut butter</label>
+                <label >Your Candy contains peanuts, almonds, or peanut butter</label>
                 <br/>
                 <input type="checkbox" name="nougat" onClick={() => {candy.nougat = 1}}></input>
-                <label for="nougat">Your Candy has nougat</label>
+                <label >Your Candy has nougat</label>
                 <br/>
                 <input type="checkbox" name="crispedricewafer" onClick={() => {candy.crispedricewafer = 1}}></input>
-                <label for="crispedricewafer">Your Candy contains crisped rice wafers</label>
+                <label >Your Candy contains crisped rice wafers</label>
                 <br/>
                 <input type="checkbox" name="hard" onClick={() => {candy.hard = 1}}></input>
-                <label for="hard">Your Candy contains is hard</label>
+                <label >Your Candy contains is hard</label>
                 <br/>
                 <input type="checkbox" name="bar" onClick={() => {candy.bar = 1}}></input>
-                <label for="bar">Your Candy is a bar</label>
+                <label >Your Candy is a bar</label>
                 <br/>
                 <input type="checkbox" name="pluribus" onClick={() => {candy.pluribus = 1}}></input>
-                <label for="pluribus">Your Candy has many peices inside 1 box</label>
+                <label >Your Candy has many peices inside 1 box</label>
                 <br/>
 
-                <label for="sugarpercent">Sugar percentage of your Candy __% (if known): </label>
+                <label >Sugar percentage of your Candy __% (if known): </label>
                 <input type="number" name="sugarpercent" onChange={(event) => {candy.sugarpercent = event.target.value/100}}></input>
                 <br/>
                 <br/>
